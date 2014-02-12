e9ac29be8f0a0fd941c92e358b3d0df06c075f8e
saasbook <saasbook@saasbook.(none)>
2014-02-11 23:51:06 -0400

    after bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClassApp::Application.config.secret_key_base = '67ff1572004bd08cbf16cca63d6db2d3bc6978a8f442693d7233d66e079978db37d75b7d2cbbd1cc9378c17b2f47386892e2c0b25aa600b45bc6f942d93811f7'
